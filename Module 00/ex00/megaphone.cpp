eca255e89d42fd92454d487762ea65193408ca97
Mohamed El Youssfi <mel-yous@e3r8p13.1337.ma>
2023-09-30 16:43:50 +0100

    Module00/ex00 is done

--- Module 00/ex00/megaphone.cpp
+++ Module 00/ex00/megaphone.cpp
@@ -6,7 +6,21 @@
 /*   By: mel-yous <mel-yous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 14:40:47 by mel-yous          #+#    #+#             */
-/*   Updated: 2023/09/30 14:40:54 by mel-yous         ###   ########.fr       */
+/*   Updated: 2023/09/30 16:39:25 by mel-yous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    else
+    {
+        for (int i = 1; i < argc; i++)
+            for (int j = 0; j < (int)std::strlen(argv[i]); j++)
+                std::cout << (char)std::toupper(argv[i][j]);
+    }
+    return (std::cout << std::endl, 0);
+}
