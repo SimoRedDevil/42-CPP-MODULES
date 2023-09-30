5e0dd4cfb1e1221428a0248a274257d8c8956f09
Mohamed El Youssfi <mel-yous@e3r8p13.1337.ma>
2023-09-30 17:27:10 +0100

    working with string class

--- Module 00/ex00/megaphone.cpp
+++ Module 00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: mel-yous <mel-yous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 14:40:47 by mel-yous          #+#    #+#             */
-/*   Updated: 2023/09/30 16:39:25 by mel-yous         ###   ########.fr       */
+/*   Updated: 2023/09/30 17:25:29 by mel-yous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,11 @@ int main(int argc, char **argv)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     else
     {
-        for (int i = 1; i < argc; i++)
-            for (int j = 0; j < (int)std::strlen(argv[i]); j++)
-                std::cout << (char)std::toupper(argv[i][j]);
+        for (int i = 1; i < argc; i++){
+            std::string str(argv[i]);   
+            for (int j = 0; j < (int)str.length(); j++)
+                std::cout << (char)std::toupper(str.at(j));
+        }
     }
     return (std::cout << std::endl, 0);
 }
